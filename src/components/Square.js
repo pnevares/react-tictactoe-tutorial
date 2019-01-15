fb1ff11b04172f4dcbac51c1fc1d026128a73613
Pablo Nevares <pablo@nevares.com>
2019-01-15 14:48:16 -0700

    Step 2: 'Lifting State Up' done

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,25 +1,9 @@
 import React from "react";
 
-export class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: props.value
-    };
-  }
-
-  setValue(value) {
-    this.setState({
-      value
-    });
-  }
-
-  render() {
-    const { value } = this.state;
-    return (
-      <button className="square" onClick={() => this.setValue("X")}>
-        {value}
-      </button>
-    );
-  }
+export function Square({ value, onClick }) {
+  return (
+    <button className="square" onClick={() => onClick("X")}>
+      {value}
+    </button>
+  );
 }
