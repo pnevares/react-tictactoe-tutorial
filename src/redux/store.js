f3ffdb0d685aab3251df7927dcfa1ea95db64b77
Pablo Nevares <pablo@nevares.com>
2019-05-11 22:32:07 -0700

    Removed empty file, removed redux devtools extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,6 @@
 import { createStore } from "redux";
 import todoApp from "./reducers";
 
-const store = createStore(
-  todoApp,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(todoApp);
 
 export default store;
